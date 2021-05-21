3f24a1b97970896d97dc24fdecbe368c799f14e8
Zhino Salam <zhinosalam188@gmail.com>
2021-05-21 21:55:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const titleCased = () => {
+  return tutorials
+  return tutorials.map( str => str.split(' ').map( word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
 }
