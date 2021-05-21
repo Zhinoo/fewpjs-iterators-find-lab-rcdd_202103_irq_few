5a558177d06d1d6c0e12327651dc11687dc8934c
Zhino Salam <zhinosalam188@gmail.com>
2021-05-21 21:55:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const titleCased = () => {
-  return tutorials
-  return tutorials.map( str => str.split(' ').map( word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+function superbowlWin(superbowls){
+  let win = superbowls.find(superbowl => superbowl.result === "W")
+  return win ? win.year : undefined;
 }
